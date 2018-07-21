d82d0a24e7fbea9e5f0ea5ddee0d0a29449fe523
Claudio Pupparo <claudio.pupparo@gmail.com>
2018-07-21 16:29:20 +0200

    Movements to get to final result are now shown

--- deponia-hook-solver/algorithm/dijsktra.py
+++ deponia-hook-solver/algorithm/dijsktra.py
@@ -3,29 +3,40 @@ import heapq as heapq
 
 
 # https://gist.github.com/kachayev/5990802
+# https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm#Using_a_priority_queue
 class Dijkstra(object):
 
+    """
+    edges = [(node1, node2, cost)]
+    f = start_node
+    t = end_node
+    """
     def solve(self, edges, f, t):
-        g = collections.defaultdict(list)
+        g = collections.defaultdict(list)  # g[k] = [] if key is missing
         for l, r, c in edges:
-            g[l].append((c, r))
+            g[l].append((c, r))  # g[l] = list of neighbors r of l with costs c
 
-        q, seen, mins = [(0, f, ())], set(), {f: 0}
+        q, seen, mins = [(0, f, (), [])], set(), {f: 0}
+        # q = list of (cost of path to v, vertex v, path to v)
+        # seen = list of seen nodes
+        # mins = cost of path from f to node k for each k
+
         while q:
-            (cost, v1, path) = heapq.heappop(q)
+            (cost, v1, path, traversed_nodes) = heapq.heappop(q)  # Pop and return the vertex with current smallest cost
             if v1 not in seen:
                 seen.add(v1)
                 path = (v1, path)
+                traversed_nodes.append(v1)
                 if v1 == t:
-                    return cost, path
+                    return cost, path, traversed_nodes
 
-                for c, v2 in g.get(v1, ()):
+                for c, v2 in g.get(v1, ()):  # list of neighbours v2 of v1 and their current cost
                     if v2 in seen:
                         continue
-                    prev = mins.get(v2, None)
+                    prev = mins.get(v2, None)  # get cost of path to v2
                     next = cost + c
                     if prev is None or next < prev:
                         mins[v2] = next
-                        heapq.heappush(q, (next, v2, path))
+                        heapq.heappush(q, (next, v2, path, list(traversed_nodes)))
 
         return float("inf")
--- deponia-hook-solver/solver.py
+++ deponia-hook-solver/solver.py
@@ -4,7 +4,6 @@ import model.grid_limit as grid_limit
 import algorithm.graph_generator as graph_generator
 import algorithm.dijsktra as dijsktra
 
-
 if __name__ == "__main__":
     start = vertex.Vertex(4, 1)
     end = vertex.Vertex(1, 3)
@@ -18,4 +17,45 @@ if __name__ == "__main__":
 
     d = dijsktra.Dijkstra()
     solution = d.solve(graph.edges, start.name, end.name)
-    print str(solution)
+    # print str(solution[0])
+    # print str(solution[1])
+    print("Path: ")
+    print str(solution[2])
+
+    traversed_nodes = solution[2]
+    vertex_map = {}
+
+    for v in graph.vertices:
+        vertex_map[v.name] = v
+
+    print("Movements to apply: ")
+    for i in range(0, len(traversed_nodes) - 1):
+        next_node = vertex_map[traversed_nodes[i + 1]]
+        cur_node = vertex_map[traversed_nodes[i]]
+
+        mov_x = next_node.x - cur_node.x
+        mov_y = next_node.y - cur_node.y
+        print("({}, {})".format(mov_x, mov_y))
+
+    # edges = [
+    #     ("A", "B", 7),
+    #     ("A", "D", 5),
+    #     ("B", "C", 8),
+    #     ("B", "D", 9),
+    #     ("B", "E", 7),
+    #     ("C", "E", 5),
+    #     ("D", "E", 15),
+    #     ("D", "F", 6),
+    #     ("E", "F", 8),
+    #     ("E", "G", 9),
+    #     ("F", "G", 11)
+    # ]
+    #
+    # d = dijsktra.Dijkstra()
+    #
+    # print "=== Dijkstra ==="
+    # print edges
+    # print "A -> E:"
+    # print d.solve(edges, "A", "E")
+    # print "F -> G:"
+    # print d.solve(edges, "F", "G")
